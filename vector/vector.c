0f6fcc86363d168c75580fe82a64872ca3deaf58
dwrz <dwrz@dwrz.net>
2019-05-25 02:12:11 +0000

    Reformat vector/vector.c

--- vector/vector.c
+++ vector/vector.c
@@ -32,17 +32,15 @@ void append_vector(Vector *v, int elem) {
 }
 
 void delete_vector(Vector *v) {
-	free(v -> data);
+	free(v->data);
 
-	v -> capacity = 0;
-	v -> size = 0;
-	v -> data = NULL;
+	v->capacity = 0;
+	v->size = 0;
+	v->data = NULL;
 }
 
 Vector new_vector(int capacity) {
-	if (capacity < 0) {
-		capacity = 0;
-	}
+	if (capacity < 0) capacity = 0;
 
 	// TODO: What if calloc() fails?
 	Vector v = {
@@ -55,16 +53,16 @@ Vector new_vector(int capacity) {
 }
 
 void print_vector(Vector *v) {
-	printf("capacity: %d\n", v -> capacity);
-	printf("size: %d\n", v -> size);
+	printf("capacity: %d\n", v->capacity);
+	printf("size: %d\n", v->size);
 
-	if (v -> data == NULL) {
+	if (v->data == NULL) {
 		printf("data: NULL\n");
 		return;
 	};
 
 	printf("data: \n");
-	for (int i = 0; i < v -> capacity; i++) {
-		printf("[%d]: %d\n", i, v -> data[i]);
+	for (int i = 0; i < v->capacity; i++) {
+		printf("[%d]: %d\n", i, v->data[i]);
 	}
 }
